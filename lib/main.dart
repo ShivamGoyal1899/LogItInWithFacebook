1af48edba6f725817c3a758a5db98a2d340f7022
Shivam Goyal <shivamgoyal1899@gmail.com>
2019-06-12 12:41:04 +0530

    added COdew

---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(LoginPage());
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLoggedIn = false;
+  var profileData;
+
+  var facebookLogin = FacebookLogin();
+
+  void onLoginStatusChanged(bool isLoggedIn, {profileData}) {
+    setState(() {
+      this.isLoggedIn = isLoggedIn;
+      this.profileData = profileData;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Facebook Login"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Colors.white,
+              ),
+              onPressed: () => facebookLogin.isLoggedIn
+                  .then((isLoggedIn) => isLoggedIn ? _logout() : {}),
+            ),
+          ],
+        ),
+        body: Container(
+          child: Center(
+            child: isLoggedIn
+                ? _displayUserData(profileData)
+                : _displayLoginButton(),
+          ),
+        ),
+      ),
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.grey[600],
+        ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.blue[500],
+        textSelectionHandleColor: Colors.blue[500],
+      ),
+    );
+  }
+
+  void initiateFacebookLogin() async {
+    var facebookLoginResult =
+    await facebookLogin.logInWithReadPermissions(['email']);
+
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        var graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email,picture.width(400)&access_token=${facebookLoginResult
+                .accessToken.token}');
+
+        var profile = json.decode(graphResponse.body);
+        print(profile.toString());
+
+        onLoginStatusChanged(true, profileData: profile);
+        break;
+    }
+  }
+
+  _displayUserData(profileData) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          height: 200.0,
+          width: 200.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                profileData['picture']['data']['url'],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 28.0),
+        Text(
+          "FBID ${profileData['id']}\n${profileData['name']}\n${profileData['email']}",
+          style: TextStyle(
+          fontSize: 20.0,
+          letterSpacing: 1.1,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  _displayLoginButton() {
+    return RaisedButton(
+      child: Text("Login with Facebook"),
+      onPressed: () => initiateFacebookLogin(),
+      color: Colors.blue,
+      textColor: Colors.white,
+    );
+  }
+
+  _logout() async {
+    await facebookLogin.logOut();
+    onLoginStatusChanged(false);
+    print("Logged out");
+  }
+}
